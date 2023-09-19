426c9bc311cb1bd4f06f0340f3248783af8bc181
aakash <punsiya.aakash@gmail.com>
2023-09-19 23:05:56 +0530

    connect server with express

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-var http = require("http");
-const hostName = "127.0.0.1";
-const port = 3000;
+// var http = require("http");
+// const hostName = "127.0.0.1";
+// const port = 3000;
 
-//create a server object:
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.write("Hello World!");
-  res.end();
-});
-server.listen(port, hostName, () => {
-  console.log(`server start at port ${port}`);
-});
+// //create a server object:
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.write("Hello World!");
+//   res.end();
+// });
+// server.listen(port, hostName, () => {
+//   console.log(`server start at port ${port}`);
+// });
+
+const express = require('express')
+const app = express()
+const port = 3000;
+app.get("/",(req,res) => {
+    res.send("Hello express call heer")
+})
+app.listen(port,()=>{
+    console.log(`Server is runnin on this Port number ${port}`);
+})
